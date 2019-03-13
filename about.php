af435cea8def4a4c8a6ccf3a6b3c50720c5b2581
tamilselvy <smtamilselvyts@gmail.com>
2019-03-13 11:39:19 +0530

    Added Menu and about.php

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
                     <h2>ABOUT US</h2>
                     <p>    We are fond of novelty. We created our own work culture which makes our coworkers to come together and entertain themselves through ambitious exploration over the developing technologies. We truly believe in the transformative control of illustration and design and their capacity to shorten communications. Thus, our ultimate aim is to achieve the tricky desire of our clients.
 
-    Smart Tech Enterprise Solutions is the leading IT solutions in the field of Digital Marketing, Hybrid & Native apps, Embedded System and Animation. We are also a premier house for providing hi-tech support solutions in the field of Information Technology(Audio and Video Editing , Hardware, Software and Data Storage Solutions includes (DAS, NAS & SAN).</p>
+                    Smart Tech Enterprise Solutions is the leading IT solutions in the field of Digital Marketing, Hybrid & Native apps, Embedded System and Animation. We are also a premier house for providing hi-tech support solutions in the field of Information Technology(Audio and Video Editing , Hardware, Software and Data Storage Solutions includes (DAS, NAS & SAN).</p>
                 </div>
             </div>
             
